c093e84b82fbccedf4e8b143e3b2f4402d054dec
Anthony Leveillee <aleveillee@gmail.com>
2025-11-27 18:30:23 -0500

    add basic style

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,77 @@
+import { Container, FormControlLabel, FormGroup } from '@mui/material';
+import Switch from '@mui/material/Switch';
 import { useState } from 'react';
-import './App.css'
 import { Chart } from 'react-google-charts';
+import './App.css';
 import { Parallel } from './parallel';
 
-const ALL_PARALLELS = new Array<Parallel>();
+const ALL_PARALLELS = [
 
-// 2014
-ALL_PARALLELS.push(Parallel.newParallel('Golden Treasures', '1/1', new Date(2014, 0, 1), new Date(2025, 0, 1), true));
-ALL_PARALLELS.push(Parallel.newParallel('Black Ice', '#/25', new Date(2014, 0, 1), new Date(2015, 0, 1), false));
-ALL_PARALLELS.push(Parallel.newParallel('Seismic Gold', '#/50', new Date(2014, 0, 1), new Date(2025, 0, 1), true));
-ALL_PARALLELS.push(Parallel.newParallel('Blue Cubes', '#/65', new Date(2014, 0, 1), new Date(2015, 0, 1), false));
-ALL_PARALLELS.push(Parallel.newParallel('Red Prism', '#/139', new Date(2014, 0, 1), new Date(2015, 0, 1), true));
-ALL_PARALLELS.push(Parallel.newParallel('White Ice', '#/199', new Date(2014, 0, 1), new Date(2016, 0, 1), false));
-ALL_PARALLELS.push(Parallel.newParallel('Rainbow Trax', 'N/A', new Date(2014, 0, 1), new Date(2015, 0, 1), false));
-ALL_PARALLELS.push(Parallel.newParallel('Rainbow', 'N/A', new Date(2014, 0, 1), new Date(2025, 0, 1), true));
+  // 2014
+  Parallel.newParallel('Golden Treasures', '1/1', new Date(2014, 0, 1), new Date(2025, 0, 1), true),
+  Parallel.newParallel('Black Ice', '#/25', new Date(2014, 0, 1), new Date(2015, 0, 1), false),
+  Parallel.newParallel('Seismic Gold', '#/50', new Date(2014, 0, 1), new Date(2025, 0, 1), true),
+  Parallel.newParallel('Blue Cubes', '#/65', new Date(2014, 0, 1), new Date(2015, 0, 1), false),
+  Parallel.newParallel('Red Prism', '#/139', new Date(2014, 0, 1), new Date(2015, 0, 1), true),
+  Parallel.newParallel('White Ice', '#/199', new Date(2014, 0, 1), new Date(2016, 0, 1), false),
+  Parallel.newParallel('Rainbow Trax', 'N/A', new Date(2014, 0, 1), new Date(2015, 0, 1), false),
+  Parallel.newParallel('Rainbow', 'N/A', new Date(2014, 0, 1), new Date(2025, 0, 1), true),
 
-// 2015
-ALL_PARALLELS.push(Parallel.newParallel('Emerald Surge', '#/10', new Date(2015, 0, 1), new Date(2025, 0, 1), true));
-ALL_PARALLELS.push(Parallel.newParallel('Purple', '#/25', new Date(2015, 0, 1), new Date(2016, 0, 1), false));
-ALL_PARALLELS.push(Parallel.newParallel('Blue Cubes', '#/75', new Date(2015, 0, 1), new Date(2016, 0, 1), false));
-ALL_PARALLELS.push(Parallel.newParallel('Black Ice', '#/99', new Date(2015, 0, 1), new Date(2016, 0, 1), false));
-ALL_PARALLELS.push(Parallel.newParallel('Red Prism', '#/149', new Date(2015, 0, 1), new Date(2016, 0, 1), true));
+  // 2015
+  Parallel.newParallel('Emerald Surge', '#/10', new Date(2015, 0, 1), new Date(2025, 0, 1), true),
+  Parallel.newParallel('Purple', '#/25', new Date(2015, 0, 1), new Date(2016, 0, 1), false),
+  Parallel.newParallel('Blue Cubes', '#/75', new Date(2015, 0, 1), new Date(2016, 0, 1), false),
+  Parallel.newParallel('Black Ice', '#/99', new Date(2015, 0, 1), new Date(2016, 0, 1), false),
+  Parallel.newParallel('Red Prism', '#/149', new Date(2015, 0, 1), new Date(2016, 0, 1), true),
 
-// 2016
-ALL_PARALLELS.push(Parallel.newParallel('Orange Checkers*', '#/25', new Date(2016, 0, 1), new Date(2025, 0, 1), true));
-ALL_PARALLELS.push(Parallel.newParallel('Blue Cubes', '#/99', new Date(2016, 0, 1), new Date(2018, 0, 1), false));
-ALL_PARALLELS.push(Parallel.newParallel('Red Prism', '#/199', new Date(2016, 0, 1), new Date(2025, 0, 1), true));
-ALL_PARALLELS.push(Parallel.newParallel('Ice Blue Traxx', 'N/A', new Date(2016, 0, 1), new Date(2017, 0, 1), false));
+  // 2016
+  Parallel.newParallel('Orange Checkers*', '#/25', new Date(2016, 0, 1), new Date(2025, 0, 1), true),
+  Parallel.newParallel('Blue Cubes', '#/99', new Date(2016, 0, 1), new Date(2018, 0, 1), false),
+  Parallel.newParallel('Red Prism', '#/199', new Date(2016, 0, 1), new Date(2025, 0, 1), true),
+  Parallel.newParallel('Ice Blue Traxx', 'N/A', new Date(2016, 0, 1), new Date(2017, 0, 1), false),
 
-// 2017
-ALL_PARALLELS.push(Parallel.newParallel('Violet Pixels', 'N/A', new Date(2017, 0, 1), new Date(2019, 0, 1), true));
+  // 2017
+  Parallel.newParallel('Violet Pixels', 'N/A', new Date(2017, 0, 1), new Date(2019, 0, 1), true),
 
-// 2018
-ALL_PARALLELS.push(Parallel.newParallel('Arctic Freeze', '#/79', new Date(2018, 0, 1), new Date(2019, 0, 1), true));
+  // 2018
+  Parallel.newParallel('Arctic Freeze', '#/79', new Date(2018, 0, 1), new Date(2019, 0, 1), true),
 
-// 2019
-ALL_PARALLELS.push(Parallel.newParallel('Arctic Freeze', '#/99', new Date(2019, 0, 1), new Date(2025, 0, 1), true));
-ALL_PARALLELS.push(Parallel.newParallel('Violet Pixels', '#/399', new Date(2019, 0, 1), new Date(2021, 0, 1), true));
-ALL_PARALLELS.push(Parallel.newParallel('Matte Pink', 'N/A', new Date(2019, 0, 1), new Date(2024, 0, 1), false));
+  // 2019
+  Parallel.newParallel('Arctic Freeze', '#/99', new Date(2019, 0, 1), new Date(2025, 0, 1), true),
+  Parallel.newParallel('Violet Pixels', '#/399', new Date(2019, 0, 1), new Date(2021, 0, 1), true),
+  Parallel.newParallel('Matte Pink', 'N/A', new Date(2019, 0, 1), new Date(2024, 0, 1), false),
 
-// 2020
-ALL_PARALLELS.push(Parallel.newParallel('Cosmic', '#/65', new Date(2020, 0, 1), new Date(2025, 0, 1), true));
-ALL_PARALLELS.push(Parallel.newParallel('Yellow Traxx', '#/249', new Date(2020, 0, 1), new Date(2021, 0, 1), false));
-ALL_PARALLELS.push(Parallel.newParallel('Aquamarine', '#/499', new Date(2020, 0, 1), new Date(2021, 0, 1), false));
-ALL_PARALLELS.push(Parallel.newParallel('Blue Surge', 'N/A', new Date(2020, 0, 1), new Date(2022, 0, 1), false));
-ALL_PARALLELS.push(Parallel.newParallel('Red Surge', 'N/A', new Date(2020, 0, 1), new Date(2022, 0, 1), false));
-ALL_PARALLELS.push(Parallel.newParallel('Yellow Surge', 'N/A', new Date(2020, 0, 1), new Date(2025, 0, 1), true));
+  // 2020
+  Parallel.newParallel('Cosmic', '#/65', new Date(2020, 0, 1), new Date(2025, 0, 1), true),
+  Parallel.newParallel('Yellow Traxx', '#/249', new Date(2020, 0, 1), new Date(2021, 0, 1), false),
+  Parallel.newParallel('Aquamarine', '#/499', new Date(2020, 0, 1), new Date(2021, 0, 1), false),
+  Parallel.newParallel('Blue Surge', 'N/A', new Date(2020, 0, 1), new Date(2022, 0, 1), false),
+  Parallel.newParallel('Red Surge', 'N/A', new Date(2020, 0, 1), new Date(2022, 0, 1), false),
+  Parallel.newParallel('Yellow Surge', 'N/A', new Date(2020, 0, 1), new Date(2025, 0, 1), true),
 
-// 2021
-ALL_PARALLELS.push(Parallel.newParallel('Violet Pixels', '#/299', new Date(2021, 0, 1), new Date(2025, 0, 1), true));
-ALL_PARALLELS.push(Parallel.newParallel('Hot Magma', '#/499', new Date(2021, 0, 1), new Date(2023, 0, 1), false));
+  // 2021
+  Parallel.newParallel('Violet Pixels', '#/299', new Date(2021, 0, 1), new Date(2025, 0, 1), true),
+  Parallel.newParallel('Hot Magma', '#/499', new Date(2021, 0, 1), new Date(2023, 0, 1), false),
 
-// 2022
-ALL_PARALLELS.push(Parallel.newParallel('Aquamarine', '#/149', new Date(2022, 0, 1), new Date(2023, 0, 1), false));
-ALL_PARALLELS.push(Parallel.newParallel('Liquid Metal', '#/349', new Date(2022, 0, 1), new Date(2025, 0, 1), true));
+  // 2022
+  Parallel.newParallel('Aquamarine', '#/149', new Date(2022, 0, 1), new Date(2023, 0, 1), false),
+  Parallel.newParallel('Liquid Metal', '#/349', new Date(2022, 0, 1), new Date(2025, 0, 1), true),
 
-// 2023
-ALL_PARALLELS.push(Parallel.newParallel('Bioluminescence', '#/5', new Date(2023, 0, 1), new Date(2025, 0, 1), true));
-ALL_PARALLELS.push(Parallel.newParallel('Blue Fragments', '#/125', new Date(2023, 0, 1), new Date(2025, 0, 1), true));
-ALL_PARALLELS.push(Parallel.newParallel('Pond Hockey', 'SSP', new Date(2023, 0, 1), new Date(2024, 0, 1), false));
+  // 2023
+  Parallel.newParallel('Bioluminescence', '#/5', new Date(2023, 0, 1), new Date(2025, 0, 1), true),
+  Parallel.newParallel('Blue Fragments', '#/125', new Date(2023, 0, 1), new Date(2025, 0, 1), true),
+  Parallel.newParallel('Pond Hockey', 'SSP', new Date(2023, 0, 1), new Date(2024, 0, 1), false),
 
-// 2024
-ALL_PARALLELS.push(Parallel.newParallel('Pink Magma', 'N/A', new Date(2024, 0, 1), new Date(2025, 0, 1), true));
+  // 2024
+  Parallel.newParallel('Pink Magma', 'N/A', new Date(2024, 0, 1), new Date(2025, 0, 1), true),
+];
 
 export default function App() {
   const [showDiscontinued, setShowDiscontinued] = useState(false);
+
+  function handleDiscontinuedChange() {
+    setShowDiscontinued(!showDiscontinued);
+  }
 
   const parallels = ALL_PARALLELS.filter((p) => showDiscontinued || p.active);
 
@@ -78,20 +85,22 @@ export default function App() {
     ...parallels.map((p) => p.toDataEntry())
   ];
 
-  return <>
-    <h2>O-Pee-Chee Platinum Hockey</h2>
+  return (
+    <Container fixed>
+      <h2>O-Pee-Chee Platinum Hockey</h2>
 
-    <button onClick={() => setShowDiscontinued(!showDiscontinued)}>
-      {showDiscontinued ? 'Hide Discontinued' : 'Show Discontinued'}
-    </button>
+      <FormGroup>
+        <FormControlLabel control={<Switch onChange={handleDiscontinuedChange}></Switch>} label="Show Discontinued"></FormControlLabel>
+      </FormGroup>
 
-    <Chart chartType='Timeline' data={data} width='1200px' height={showDiscontinued ? '1260px' : '630px'}
-      options={{
-        timeline: { singleColor: 'black' }
-      }} />
+      <Chart chartType='Timeline' data={data} width='1200px' height={showDiscontinued ? '1260px' : '630px'}
+        options={{
+          timeline: { fontName: 'Roboto', singleColor: 'gray' }
+        }} />
 
-    <p>
-      *: Technically, the parallel was called Rainbow Orange in 2016 but I've combined them for now.
-    </p>
-  </>
+      <p>
+        *: Technically, the parallel was called Rainbow Orange in 2016 but I've combined them for now.
+      </p>
+    </Container>
+  )
 }
